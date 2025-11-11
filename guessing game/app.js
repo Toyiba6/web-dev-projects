b64df98781e4ba8242004c405d672935221dfe5e
Toyiba <118802453+Toyiba6@users.noreply.github.com>
2025-11-11 12:14:34 +0530

    Add files via upload

---
+++ guessing game/app.js
@@ -0,0 +1,17 @@
+const max = prompt("Enter the maximum number:");
+const random = Math.floor(Math.random() * max) + 1;
+let guess = prompt ("guess the number");
+while(true){
+    if(guess == "quit"){
+        console.log("You have quit the game.");
+        break;
+    }
+    if(guess == random){
+        console.log("Congratulations! You guessed the number.", random);
+        break;
+    } else if(guess < random){
+        guess = prompt("Too low! Try again.");
+    } else {
+        guess = prompt("Too high! Try again.");
+    }
+}
